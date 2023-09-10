ea864d09d761b3b0ca6dae69e565fd5f298e1ff5
Wasim <wasimxaman13@gmail.com>
2023-09-10 16:31:00 +0500

    Operators, and variables

---
+++ Part 1 C++ Essentials/addition of two numbers.cpp
@@ -0,0 +1,14 @@
+// Sairatec
+
+// Date: 9-9-2023
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // A program that will add two numbers......
+    cout<<"hello world";
+    return 0;
+}
---
+++ Part 1 C++ Essentials/operators_variables_and_expressions.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int num1 = 13, num2 = 2;
+	float result = 0.0f;
+	
+	// I want to divide two numbers and i want to store its result in separate variable which is result
+	
+	result = (float) num1 / num2;		// 6.5
+	cout << result;
+	cout << endl;
+	float a = 13, b = 2;
+	float c = 0.0f;
+	
+	c = a / b;
+	
+	cout << c;
+	return 0;
+}
